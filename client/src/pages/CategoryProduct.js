fb61a8bb70a1729fe3dc42d3bb7a3862c7e3513b
Rajveer Kumar <sharmarajveer8287631167@gmail.com>
2025-07-21 19:21:04 +0530

    product filter out by category

--- client/src/pages/CategoryProduct.js
+++ client/src/pages/CategoryProduct.js
@@ -2,11 +2,12 @@ import { useState, useEffect, useCallback } from "react";
 import Layout from "../components/Layout/Layout";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 export const CategoryProduct = () => {
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState({});
   const params = useParams();
+  const navigate=useNavigate();
 
   const getProductsBycat = useCallback(async () => {
     try {
@@ -24,27 +25,53 @@ export const CategoryProduct = () => {
 
   return (
     <Layout title={"products"}>
-      <div className="container">
-        <h1 className="text-center">{category?.name}</h1>
-        <div className="row">
-          {products?.map((p) => (
-            <div className="col-md-4" key={p._id}>
-              <div className="card m-2">
+      <div className="container mt-2 mb-8  pb-5">
+          <h1 className="text-center mb-4">Category - {category?.name}</h1>
+          
+          <div className=" d-flex flex-wrap justify-content-start ">
+            {products?.map((p) => (
+              <div className="card m-2" style={{ width: "18rem" }} key={p._id}>
                 <img
                   src={`/api/v1/product/product-photo/${p._id}`}
                   className="card-img-top"
                   alt={p.name}
+                  style={{ height: "200px", objectFit: "cover" }}
                 />
                 <div className="card-body">
                   <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">{p.description?.substring(0, 30)}...</p>
-                  <p className="card-text">₹ {p.price}</p>
+                  <p className="card-text">
+                    {p.description?.substring(0, 30)}...
+                  </p>
+                  <p className="card-text">${p.price}</p>
+                  <div className="d-flex">
+                    <button
+                      className="btn btn-primary ms-2 "
+                      onClick={() => navigate(`/product/${p.slug}`)}
+                    >
+                      More Details
+                    </button>
+                    <button className="btn btn-secondary ms-1">
+                      ADD TO CART
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+
+          
+          {/* <div className="text-center mt-4 mb-5">
+            <p>Total Products: {total}</p>
+            {products.length < total && (
+              <button
+                className="btn btn-warning px-4 mt-2"
+                onClick={() => setPage(page + 1)}
+              >
+                Load More
+              </button>
+            )}
+          </div> */}
         </div>
-      </div>
     </Layout>
   );
 };
