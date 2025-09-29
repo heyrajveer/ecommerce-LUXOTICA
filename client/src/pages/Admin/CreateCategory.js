9686a044722a4f125f2a8b31b11b60edd7dba9a4
Rajveer Kumar <sharmarajveer8287631167@gmail.com>
2025-09-29 16:45:45 +0530

    Temp rename CategoryForm

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
-import CategoryForm from "../../components/Form/CategoryForm";
+import CategoryForm from "../../components/Form/CategoryFormTemp";
 import { Modal } from "antd";
 const BASE_URL = process.env.REACT_APP_BACKEND_URL;
 const CreateCategory = () => {
